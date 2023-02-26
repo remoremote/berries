e11e609f186968b0aefe54b187b8fa501f75f05b
Remo <81964236+remoremote@users.noreply.github.com>
2023-02-26 16:54:01 +0100

    hamburger struggles

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ $( document ).ready(function() {
     $( ".cross" ).hide();
     $( ".menu" ).hide();
     $( ".hamburger" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-    $( ".hamburger" ).hide();
-    $( ".cross" ).show();
-    });
+        $( ".menu" ).css({
+            'left': '0',
+            'right': '0'
+        });
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".hamburger" ).hide();
+            $( ".cross" ).show();
+        });
     });
     
     $( ".cross" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-    $( ".cross" ).hide();
-    $( ".hamburger" ).show();
-    });
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
     });
     
-    });
+});
